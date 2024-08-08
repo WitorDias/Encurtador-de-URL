16990aaec219ad9ada55e03ace0dc85fdc565945
WitorDias <joao_witor_dias@hotmail.com>
2024-08-08 20:51:28 -0300

    Initial commit

---
+++ src/main/java/dev/witor/encurtador/url/EncurtadorDeUrlApplication.java
@@ -0,0 +1,13 @@
+package dev.witor.encurtador.url;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtadorDeUrlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtadorDeUrlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/witor/encurtador/url/EncurtadorDeUrlApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.witor.encurtador.url;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtadorDeUrlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
